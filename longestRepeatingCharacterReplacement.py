3a238ae09cd27948b1f2d2122406b75438c5ef39
Sean Wong <sean.uuong@gmail.com>
2023-08-05 15:14:37 -0700

    Complete longestRepeatingCharacterReplacement

---
+++ longestRepeatingCharacterReplacement.py
@@ -0,0 +1,22 @@
+# Leetcode 424
+
+def characterReplacement(string, replacements):
+  counts = {}
+  counter = 0
+  left = 0
+
+  for right in range(len(string)):
+    if string[right] in counts:
+      counts[string[right]] += 1
+    else:
+      counts[string[right]] = 1
+
+    counter = counter if counter >= counts[string[right]] else counts[string[right]]
+
+    if (right - left + 1) - counter > replacements:
+      counts[string[left]] -= 1
+      left += 1
+
+  return (right - left + 1)
+
+print(characterReplacement("ABAB", 2))
