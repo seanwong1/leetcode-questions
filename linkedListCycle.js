7cd9316cd39447458c690cf1e64ea3e4f101be87
Sean Wong <sean.uuong@gmail.com>
2023-08-10 09:12:31 -0700

    Complete linkedListCycle

---
+++ linkedListCycle.js
@@ -0,0 +1,14 @@
+// Leetcode 141
+
+var hasCycle = function(head) {
+  var slow = head;
+  var fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+};
