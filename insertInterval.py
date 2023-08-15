7dca2364f87930b65981f33e07fff93d453f3b7b
Sean Wong <sean.uuong@gmail.com>
2023-08-15 16:16:38 -0700

    Complete insertInterval

---
+++ insertInterval.py
@@ -0,0 +1,22 @@
+# Leetcode 57
+
+def insert(intervals, newInterval):
+  result = []
+
+  for i in range(len(intervals)):
+    if newInterval[1] < intervals[i][0]:
+      result.append(newInterval)
+      return result + intervals[i:]
+    elif newInterval[0] > intervals[i][1]:
+      result.append(intervals[i])
+    else:
+      newInterval[0] = newInterval[0] if newInterval[0] < intervals[i][0] else intervals[i][0]
+      newInterval[1] = newInterval[1] if newInterval[1] > intervals[i][1] else intervals[i][1]
+
+  result.append(newInterval)
+
+  return result
+
+intervals = [[1,5]]
+newInterval = [6, 8]
+print(insert(intervals, newInterval))
