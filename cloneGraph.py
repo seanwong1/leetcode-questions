899a83c77e6ff9c64689eafd175653c23ac03edd
Sean Wong <sean.uuong@gmail.com>
2023-08-22 13:47:32 -0700

    Complete cloneGraph

--- cloneGraph.py
+++ cloneGraph.py
@@ -22,17 +22,3 @@ class Solution:
       return storage[node]
 
     return helper(node)
-
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-node4 = Node(4)
-
-node1.neighbors = [node2, node4]
-node2.neighbors = [node1, node3]
-node3.neighbors = [node2, node4]
-node4.neighbors = [node1, node3]
-
-s = Solution()
-
-print(s.cloneGraph(node1))
