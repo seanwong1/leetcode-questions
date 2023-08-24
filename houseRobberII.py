874e711b024ede4d92617402b5fc18da93567c8d
Sean Wong <sean.uuong@gmail.com>
2023-08-23 19:11:11 -0700

    Complete houseRobberII

---
+++ houseRobberII.py
@@ -0,0 +1,26 @@
+# Leetcode 213
+
+from typing import *
+
+class Solution:
+  def rob(self, nums: List[int]) -> int:
+    if len(nums) == 1:
+      return nums[0]
+    def helper(houses):
+      oldMax = 0
+      newMax = 0
+
+      for i in range(len(houses)):
+        temp = oldMax + houses[i] if oldMax + houses[i] > newMax else newMax
+        oldMax = newMax
+        newMax = temp
+
+      return newMax
+
+    lastHouse = helper(nums[1:])
+    firstHouse = helper(nums[0:-1])
+    return firstHouse if firstHouse >= lastHouse else lastHouse
+
+s = Solution()
+nums = [1]
+print(s.rob(nums))
