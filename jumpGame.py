89e1b0716cc29e07c2538cde0a5bef75248faaad
Sean Wong <sean.uuong@gmail.com>
2023-08-25 12:52:21 -0700

    Fix time complexity

--- jumpGame.py
+++ jumpGame.py
@@ -4,13 +4,11 @@ from typing import *
 
 class Solution:
   def canJump(self, nums: List[int]) -> bool:
-    if len(nums) == 1:
-      return True
-    else:
-      for jump in range(len(nums) - 1):
-        if nums[jump] + jump >= len(nums) - 1:
-          return self.canJump(nums[0:jump + 1])
-      return False
+    jumpTo = len(nums) - 1
+    for jump in range(len(nums) - 2, -1, -1):
+      if nums[jump] + jump >= jumpTo:
+        jumpTo = jump
+    return True if jumpTo == 0 else False
 
 s = Solution()
 nums = [2,3,1,0,4]
