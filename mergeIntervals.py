6823a1fd5c7b98b92f98ec66dcabfb2d9f6bd885
Sean Wong <sean.uuong@gmail.com>
2023-08-26 14:04:26 -0700

    Refactor solution

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -4,27 +4,20 @@ from typing import *
 
 class Solution:
   def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-    intervals.sort(key=lambda pair: pair[0])
-    newIntervals = []
-    counter = 1
-    if len(intervals) == 1:
-      return intervals
-    while counter <= len(intervals):
-      if not len(newIntervals):
-        start1, end1 = intervals[counter - 1]
-      else:
-        start1, end1 = newIntervals[-1]
-      start2, end2 = intervals[counter]
-      if end1 >= start2:
-        newIntervals.append([min(start1, start2), max(end1, end2)])
-        counter += 1
-      else:
-        newIntervals.append(intervals[counter - 1])
-        if counter == len(intervals) - 1:
-          newIntervals.append(intervals[counter])
-      counter += 1
+    intervals.sort(key=lambda interval: interval[0])
+    newIntervals = [intervals[0]]
+
+    for interval in intervals:
+      start1, end1 = interval
+      start2, end2 = newIntervals[-1]
+
+      if end2 >= start1:
+        newIntervals[-1][1] = end1
+      elif start1 > end2:
+        newIntervals.append(interval)
+
     return newIntervals
 
-intervals = [[1,4],[0,2],[3,5]]
+intervals = [[1,4],[2,3]]
 s = Solution()
 print(s.merge(intervals))
