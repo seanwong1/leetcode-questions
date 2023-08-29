cde9775e071f5a890c8652ea210cc52b04b41bd2
Sean Wong <sean.uuong@gmail.com>
2023-08-29 06:39:15 -0700

    Complete minCostClimbingStairs

---
+++ minCostClimbingStairs.js
@@ -0,0 +1,9 @@
+// Leetcode 746
+
+var minCostClimbingStairs = function(cost) {
+  cost.push(0);
+  for (var i = cost.length - 3; i >= 0; i--) {
+    cost[i] += (cost[i + 1] < cost[i + 2]) ? cost[i + 1] : cost[i + 2];
+  }
+  return Math.min(cost[0], cost[1]);
+};
