3706a06dbe0cecc160765297ceab2642f1673b17
Sean Wong <sean.uuong@gmail.com>
2023-09-02 06:58:55 -0700

    Add Stack

---
+++ Stack.js
@@ -0,0 +1,47 @@
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    var newNode = new Node(value);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var currentNode = this.first;
+      this.first = newNode;
+      newNode.next = currentNode;
+    }
+    this.size++;
+    return this;
+  }
+
+  pop() {
+    var toReturn;
+    if (this.size === 0) {
+      return null;
+    } else {
+      toReturn = this.first;
+      this.first = this.first.next;
+      toReturn.next = null;
+      this.size--;
+    }
+    return toReturn;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+var stack = new Stack();
+stack.push(5);
+stack.push(6);
+stack.push(7);
+stack.pop();
