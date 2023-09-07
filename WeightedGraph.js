60d4a4fed9454f4b2f8de0b6c7934846a2f2077a
Sean Wong <sean.uuong@gmail.com>
2023-09-07 14:14:13 -0700

    Add WeightedGraph

---
+++ WeightedGraph.js
@@ -0,0 +1,16 @@
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+}
