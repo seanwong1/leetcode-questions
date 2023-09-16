381a95bb081bc82b2d780e1063e3105b4dcc06e6
Sean Wong <sean.uuong@gmail.com>
2023-09-16 12:21:34 -0700

    Ideal solution

--- palindromicSubstrings.py
+++ palindromicSubstrings.py
@@ -4,16 +4,16 @@ class Solution:
   def countSubstrings(self, s: str) -> int:
     count = 0
     for i in range(len(s)):
-      counter = 0
-      while i - counter >= 0 and i + counter < len(s) and s[i - counter] == s[i + counter]:
-        if s[i - counter] == s[i + counter]:
-          count += 1
-          counter += 1
-      counter = 0
-      while i - counter >= 0 and i + 1 + counter < len(s) and s[i - counter] == s[i + 1 + counter]:
-        if s[i - counter] == s[i + 1 + counter]:
-          count += 1
-          counter += 1
+      count += self.countPalindromes(s, i, i)
+      count += self.countPalindromes(s, i, i + 1)
+    return count
+
+  def countPalindromes(self, s, left, right):
+    count = 0
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+      count += 1
+      left -= 1
+      right += 1
     return count
 
 s = Solution()
