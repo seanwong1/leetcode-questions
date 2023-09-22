913488a52d6e5c36d8d7e2fc83d47f6982778277
Sean Wong <sean.uuong@gmail.com>
2023-09-21 19:59:02 -0700

    Decrease runtime

--- removeDuplicatesFromSortedArray.py
+++ removeDuplicatesFromSortedArray.py
@@ -4,13 +4,11 @@ from typing import List
 
 class Solution:
   def removeDuplicates(self, nums: List[int]) -> int:
-    k, left, right = 1, 0, 1
+    left = 1
 
-    while right < len(nums):
-      if not nums[left] == nums[right]:
+    for right in range(left, len(nums)):
+      if not nums[right] == nums[right - 1]:
+        nums[left] = nums[right]
         left += 1
-        [nums[left], nums[right]] = [nums[right], nums[left]]
-        k += 1
-      right += 1
 
-    return k
+    return left
