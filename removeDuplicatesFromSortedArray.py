da867f2cfb5b5953c2189131bc31f60c23dbdd9b
Sean Wong <sean.uuong@gmail.com>
2023-09-21 19:44:25 -0700

    Complete removeDuplicatesFromSortedArray

--- removeDuplicatesFromSortedArray.py
+++ removeDuplicatesFromSortedArray.py
@@ -4,3 +4,13 @@ from typing import List
 
 class Solution:
   def removeDuplicates(self, nums: List[int]) -> int:
+    k, left, right = 1, 0, 1
+
+    while right < len(nums):
+      if not nums[left] == nums[right]:
+        left += 1
+        [nums[left], nums[right]] = [nums[right], nums[left]]
+        k += 1
+      right += 1
+
+    return k
