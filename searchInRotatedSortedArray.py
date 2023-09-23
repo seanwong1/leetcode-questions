5d65c5c6a97f9206d759f788f64714e182f109ac
Sean Wong <sean.uuong@gmail.com>
2023-09-23 09:37:37 -0700

    Complete searchInRotatedSortedArray

--- searchInRotatedSortedArray.py
+++ searchInRotatedSortedArray.py
@@ -1,21 +1,25 @@
 # Leetcode 33
 
-def search(nums, target):
-  left = 0
-  right = len(nums) - 1
-  while left <= right:
-    midpoint = (right + left) // 2
-    if nums[midpoint] == target:
-      return midpoint
-    if nums[left] < nums[midpoint]:
-      if target < nums[left]:
-        left = midpoint + 1
+from typing import List
+
+class Solution:
+  def search(self, nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+      midpoint = (right + left) // 2
+      if nums[midpoint] == target:
+        return midpoint
+      if nums[left] <= nums[midpoint]:
+        if nums[left] <= target and nums[midpoint] >= target:
+          right = midpoint - 1
+        else:
+          left = midpoint + 1
       else:
-        right = midpoint - 1
-    elif nums[midpoint] < nums[right]:
-      if target < nums[midpoint]:
-        right = midpoint - 1
-      else:
-        left = midpoint + 1
-    else:
-      return -1
+        if nums[right] >= target and nums[midpoint] <= target:
+          left = midpoint + 1
+        else:
+          right = midpoint - 1
+
+    return -1
