48296da172a4933459011ce7505e5933f2943b1d
Sean Wong <sean.uuong@gmail.com>
2023-09-23 10:19:52 -0700

    Use native find

--- findTheIndexOfTheFirstOccurrenceInAString.py
+++ findTheIndexOfTheFirstOccurrenceInAString.py
@@ -2,7 +2,4 @@
 
 class Solution:
   def strStr(self, haystack: str, needle: str) -> int:
-    for i in range(0, len(haystack) - len(needle) + 1):
-      if haystack[i:i + len(needle)] == needle:
-        return i
-    return -1
+    return haystack.find(needle)
