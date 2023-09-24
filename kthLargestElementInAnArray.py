897c9beb4e46f64b5895e4e0384a37278e1893ee
Sean Wong <sean.uuong@gmail.com>
2023-09-24 11:36:49 -0700

    Without sorting

--- kthLargestElementInAnArray.py
+++ kthLargestElementInAnArray.py
@@ -1,8 +1,11 @@
 # Leetcode 215
 
 from typing import List
+from heapq import heapify, heappop, heappush
 
 class Solution:
   def findKthLargest(self, nums: List[int], k: int) -> int:
-    nums = sorted(nums)
-    return nums[len(nums) - k]
+    heapify(nums)
+    while len(nums) > k:
+      heappop(nums)
+    return nums[0]
