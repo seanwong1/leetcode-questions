754540448cfe904b824e6c99fbd05ea7451b95c0
Sean Wong <sean.uuong@gmail.com>
2023-09-24 09:46:33 -0700

    Add setMatrixZeroes

---
+++ setMatrixZeroes.py
@@ -0,0 +1,9 @@
+# Leetcode 73
+
+from typing import List
+
+class Solution:
+  def setZeroes(self, matrix: List[List[int]]) -> None:
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
