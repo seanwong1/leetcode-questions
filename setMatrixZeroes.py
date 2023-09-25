9d37d92c4c38fcc2367523bebcf55e42709b11fa
Sean Wong <sean.uuong@gmail.com>
2023-09-25 07:06:31 -0700

    Complete setMatrixZeroes

--- setMatrixZeroes.py
+++ setMatrixZeroes.py
@@ -4,6 +4,19 @@ from typing import List
 
 class Solution:
   def setZeroes(self, matrix: List[List[int]]) -> None:
-    """
-    Do not return anything, modify matrix in-place instead.
-    """
+    WIDTH = len(matrix[0])
+    HEIGHT = len(matrix)
+    storage = {}
+    for i in range(HEIGHT):
+      for j in range(WIDTH):
+        if matrix[i][j] == 0:
+          if not i in storage:
+            storage[i] = [j]
+          else:
+            storage[i].append(j)
+
+    for row in storage:
+      matrix[row] = [0] * WIDTH
+      for column in storage[row]:
+        for i in range(HEIGHT):
+          matrix[i][column] = 0
