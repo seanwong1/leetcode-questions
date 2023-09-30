1fb0cac5cd99d7ddefcf3bf6ffd556170286fe0a
Sean Wong <sean.uuong@gmail.com>
2023-09-29 18:43:51 -0700

    Complete mergesortedArray

--- mergeSortedArray.py
+++ mergeSortedArray.py
@@ -4,6 +4,17 @@ from typing import List
 
 class Solution:
   def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-    """
-    Do not return anything, modify nums1 in-place instead.
-    """
+    n = n - 1
+    m = m - 1
+    mergedLength = len(nums1) - 1
+
+    while n >= 0:
+      if nums1[m] > nums2[n] and m >= 0:
+        [nums1[mergedLength], nums1[m]] = [nums1[m], nums1[mergedLength]]
+        m -= 1
+      else:
+        nums1[mergedLength] = nums2.pop()
+        n -= 1
+      mergedLength -= 1
+
+    return nums1
