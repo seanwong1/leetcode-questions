b337904f6957f25141cfed0c721ea05312b54b6b
Sean Wong <sean.uuong@gmail.com>
2023-10-03 18:32:14 -0700

    Complete minimumDepthOfBinaryTree

--- minimumDepthOfBinaryTree.py
+++ minimumDepthOfBinaryTree.py
@@ -10,3 +10,13 @@ class TreeNode:
 
 class Solution:
   def minDepth(self, root: Optional[TreeNode]) -> int:
+    if not root:
+      return 0
+    elif not root.left and not root.right:
+      return 1
+    elif not root.left:
+      return self.minDepth(root.right) + 1
+    elif not root.right:
+      return self.minDepth(root.left) + 1
+    else:
+      return min(self.minDepth(root.right), self.minDepth(root.left)) + 1
