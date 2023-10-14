7e2b43748e2568fb07645f8278f48d76d07f2442
Sean Wong <sean.uuong@gmail.com>
2023-10-13 19:18:02 -0700

    Complete excelSheetColumnTitle

---
+++ excelSheetColumnTitle.py
@@ -0,0 +1,19 @@
+# Leetcode 168
+
+from typing import Tuple
+
+class Solution:
+  def convertToTitle(self, columnNumber: int) -> str:
+    title = []
+    while columnNumber > 0:
+      columnNumber, letter = self.toBase(columnNumber, 26)
+      title.append(chr(letter + 64))
+    return ''.join(title[::-1])
+
+  def toBase(self, n: int, base: int) -> Tuple[int, int]:
+    position, remainder = divmod(n, base)
+
+    if remainder == 0:
+      return position - 1, remainder + base
+
+    return position, remainder
