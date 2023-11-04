a7b88fa3110c1674652959fdaad276c6150d69e7
Sean Wong <sean.uuong@gmail.com>
2023-11-04 07:21:07 -0700

    Use reverse method

--- reverseString.py
+++ reverseString.py
@@ -4,5 +4,4 @@ from typing import List
 
 class Solution:
   def reverseString(self, s: List[str]) -> None:
-    for i in range(len(s) // 2):
-      [s[i], s[-i - 1]] = [s[-i - 1], s[i]]
+    s.reverse()
