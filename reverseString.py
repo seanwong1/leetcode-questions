0ef8e3a5bbd49a45346b60797740d08ff5a47238
Sean Wong <sean.uuong@gmail.com>
2023-11-04 07:19:53 -0700

    Complete reverseString

---
+++ reverseString.py
@@ -0,0 +1,8 @@
+# Leetcode 344
+
+from typing import List
+
+class Solution:
+  def reverseString(self, s: List[str]) -> None:
+    for i in range(len(s) // 2):
+      [s[i], s[-i - 1]] = [s[-i - 1], s[i]]
