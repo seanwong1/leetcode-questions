dcec168ba0be2a6978f2c645748f02f0d9476341
Sean Wong <sean.uuong@gmail.com>
2023-11-05 10:11:28 -0800

    Improve runtime

--- reverseVowelsOfAString.py
+++ reverseVowelsOfAString.py
@@ -2,22 +2,14 @@
 
 class Solution:
   def reverseVowels(self, s: str) -> str:
-    newStr = [None] * len(s)
-    pointer1 = 0
-    pointer2 = len(s) - 1
+    vowelStr = []
     vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    newStr = [char if not char in vowels else vowelStr.append(char) for char in s]
 
-    while pointer1 <= pointer2:
-      if s[pointer1] in vowels and s[pointer2] in vowels:
-        newStr[pointer1] = s[pointer2]
-        newStr[pointer2] = s[pointer1]
-        pointer1 += 1
-        pointer2 -= 1
-      elif not s[pointer1] in vowels:
-        newStr[pointer1] = s[pointer1]
-        pointer1 += 1
-      elif not s[pointer2] in vowels:
-        newStr[pointer2] = s[pointer2]
-        pointer2 -= 1
+    vowelStr.reverse()
+
+    for i in range(len(newStr) - 1, -1, -1):
+      if newStr[i] == None:
+        newStr[i] = vowelStr.pop()
 
     return ''.join(newStr)
