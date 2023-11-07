322814c2c5f5ec7869b96421511f83ed0b2cabec
Sean Wong <sean.uuong@gmail.com>
2023-11-06 20:49:53 -0800

    Use set intersection

--- intersectionOfTwoArrays.py
+++ intersectionOfTwoArrays.py
@@ -4,10 +4,4 @@ from typing import List
 
 class Solution:
   def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-    result = set()
-
-    for num in nums1:
-      if num in nums2:
-        result.add(num)
-
-    return list(result)
+    return list(set(nums1).intersection(set(nums2)))
