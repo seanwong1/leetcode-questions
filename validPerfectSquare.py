3ecf09ec763bd6416a9a08d995ada0274d3db54a
Sean Wong <sean.uuong@gmail.com>
2023-11-08 06:59:12 -0800

    Without loop

--- validPerfectSquare.py
+++ validPerfectSquare.py
@@ -2,14 +2,4 @@
 
 class Solution:
   def isPerfectSquare(self, num: int) -> bool:
-    if num == 0:
-      return True
-
-    i = 1
-    while i * i <= num:
-      if num % i == 0 and num / i == i:
-        return True
-
-      i += 1
-
-    return False
+    return (num**(1 / 2)).is_integer()
