98d1893e7bf1c1bf25426aeced50ee00b3962f77
Sean Wong <sean.uuong@gmail.com>
2023-11-12 12:52:17 -0800

    Use sort with key

--- kClosestPointsToOrigin.py
+++ kClosestPointsToOrigin.py
@@ -4,22 +4,10 @@ from typing import List
 
 class Solution:
   def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-    distances = {}
-    result = []
+    def distance(point: List[int]) -> float:
+      x, y = point
+      return (x ** 2 + y ** 2) ** (1 / 2)
 
-    for point in points:
-      distance = ((abs(point[0]) ** 2) + (abs(point[1]) ** 2)) ** (1 / 2)
-      if not distance in distances:
-        distances[distance] = [point]
-      else:
-        distances[distance].append(point)
+    points.sort(key=distance)
 
-    for i in sorted(distances.keys()):
-      while k > 0 and len(distances[i]):
-        result.append(distances[i].pop())
-        k -= 1
-
-      if k <= 0:
-        break
-
-    return result
+    return points[:k]
