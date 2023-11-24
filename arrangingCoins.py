72a5d83fdb33c481ef82d393ab3e18be1af242ea
Sean Wong <sean.uuong@gmail.com>
2023-11-24 08:22:42 -0800

    Using quadratic formula

--- arrangingCoins.py
+++ arrangingCoins.py
@@ -2,8 +2,4 @@
 
 class Solution:
   def arrangeCoins(self, n: int) -> int:
-    for num in range(1, n + 1):
-      if num + (num ** 2) > n * 2:
-        return num - 1
-
-    return 1
+    return int((2 ** (1 / 2)) * ((n + (1 / 8)) ** (1 / 2)) - .5)
