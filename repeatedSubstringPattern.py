ecac7c7b0d3e7c61bf7b76f00adfd9422cb4883e
Sean Wong <sean.uuong@gmail.com>
2023-11-28 07:06:07 -0800

    Improve runtime

--- repeatedSubstringPattern.py
+++ repeatedSubstringPattern.py
@@ -2,8 +2,4 @@
 
 class Solution:
   def repeatedSubstringPattern(self, s: str) -> bool:
-    for i in range(1, (len(s) // 2) + 1):
-      for j in range(2, (len(s) // i) + 1):
-        if s[0:i] * j == s:
-          return True
-    return False
+    return True if s in s * 2[1:-1] else False
