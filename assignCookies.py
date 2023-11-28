1077da1945dd2e5753883faa0eec2bbcd8d496d0
Sean Wong <sean.uuong@gmail.com>
2023-11-27 17:52:38 -0800

    Use for loop

--- assignCookies.py
+++ assignCookies.py
@@ -4,16 +4,14 @@ from typing import List
 
 class Solution:
   def findContentChildren(self, g: List[int], s: List[int]) -> int:
-    content = 0
     g.sort()
     s.sort()
     child = 0
-    cookie = 0
 
-    while child < len(g) and cookie < len(s):
-      if s[cookie] >= g[child]:
-        content += 1
+    for cookie in s:
+      if cookie >= g[child]:
         child += 1
-      cookie += 1
+      if child > len(g) - 1:
+        break
 
-    return content
+    return child
