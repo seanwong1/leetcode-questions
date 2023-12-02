9ab014edac81e116c6f5acdedd046eb5caaa97a1
Sean Wong <sean.uuong@gmail.com>
2023-12-02 09:07:14 -0800

    Clean up logic

--- licenseKeyFormatting.py
+++ licenseKeyFormatting.py
@@ -2,7 +2,7 @@
 
 class Solution:
   def licenseKeyFormatting(self, s: str, k: int) -> str:
-    s = ''.join(s.split('-'))[::-1]
+    s = ''.join(s.split('-'))[::-1].upper()
     counter = 0
     result = ''
 
@@ -10,11 +10,7 @@ class Solution:
       if counter == k:
         result += '-'
         counter = 0
-      if char.isalpha():
-        result += char.upper()
-        counter += 1
-      else:
-        result += char
-        counter += 1
+      result += char
+      counter += 1
 
     return result[::-1]
