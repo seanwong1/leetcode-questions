3eca34fd84111ae9af3e0103882c672922f81473
Sean Wong <sean.uuong@gmail.com>
2023-12-15 21:10:11 -0800

    Complete minimumAbsoluteDifferenceInBST

---
+++ minimumAbsoluteDifferenceInBST.py
@@ -0,0 +1,30 @@
+# Leetcode 530
+
+from typing import Optional
+
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+class Solution:
+  def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+    storage = []
+    minimum = float('inf')
+
+    def helper(node: Optional[TreeNode]) -> None:
+      if node == None:
+        return
+
+      helper(node.left)
+      storage.append(node.val)
+      helper(node.right)
+
+    helper(root)
+    storage = sorted(storage)
+
+    for i in range(len(storage) - 1):
+      minimum = min(minimum, abs(storage[i] - storage[i + 1]))
+
+    return minimum
