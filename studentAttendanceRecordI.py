92ec0a561b2850cc9cfcb1e0027e93b177d799ed
Sean Wong <sean.uuong@gmail.com>
2023-12-19 19:09:01 -0800

    Cleaner

--- studentAttendanceRecordI.py
+++ studentAttendanceRecordI.py
@@ -2,20 +2,4 @@
 
 class Solution:
   def checkRecord(self, s: str) -> bool:
-    late = 0
-    absence = 0
-
-    for attendance in s:
-      if attendance == 'L':
-        late += 1
-        if late >= 3:
-          return False
-      elif attendance == 'A':
-        absence += 1
-        if absence >= 2:
-          return False
-        late = 0
-      else:
-        late = 0
-
-    return True
+    return s.count('A') < 2 and 'LLL' not in s
