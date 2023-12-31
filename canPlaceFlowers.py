60517495a516c1a478624820c824964aaa08d868
Sean Wong <sean.uuong@gmail.com>
2023-12-31 11:58:11 -0800

    Complete canPlaceFlowers

---
+++ canPlaceFlowers.py
@@ -0,0 +1,18 @@
+# Leetcode 605
+
+from typing import List
+
+class Solution:
+  def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    for i in range(len(flowerbed)):
+      if flowerbed[i] == 0 and n > 0:
+        flowerbed[i] = 1
+        n -= 1
+        if i > 0 and flowerbed[i - 1] != 0:
+          flowerbed[i] = 0
+          n += 1
+        elif i < len(flowerbed) - 1 and flowerbed[i + 1] != 0:
+          flowerbed[i] = 0
+          n += 1
+
+    return n == 0
