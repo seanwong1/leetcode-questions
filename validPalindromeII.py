ca7a990fdeeafb6fb1d8d9bcc9a1c474377e09b4
Sean Wong <sean.uuong@gmail.com>
2024-01-15 12:10:22 -0800

    Complete validPalindromeII

---
+++ validPalindromeII.py
@@ -0,0 +1,21 @@
+# Leetcode 680
+
+class Solution:
+  def validPalindrome(self, s: str) -> bool:
+    if s == s[::-1]:
+      return True
+
+    left = 0
+    right = len(s) - 1
+
+    while left <= right:
+      if s[left] != s[right]:
+        string1 = s[:left] + s[left + 1:]
+        string2 = s[:right] + s[right + 1:]
+
+        return string1 == string1[::-1] or string2 == string2[::-1]
+
+      left += 1
+      right -= 1
+
+    return True
