9a8610207c143c13323f4d08f56c955a2f1fbdfd
Sean Wong <sean.uuong@gmail.com>
2024-03-24 07:08:30 -0700

    Complete heightChecker

---
+++ heightChecker.py
@@ -0,0 +1,14 @@
+# Leetcode 1051
+
+from typing import List
+
+class Solution:
+  def heightChecker(self, heights: List[int]) -> int:
+    expected = sorted(heights)
+    counter = 0
+
+    for i in range(len(heights)):
+      if heights[i] != expected[i]:
+        counter += 1
+
+    return counter
