d3e9cf8eb157826b9f4dfd2403112f138bd211fd
Sean Wong <sean.uuong@gmail.com>
2024-04-10 17:06:18 -0700

    Complete uniqueNumberOfOccurences

---
+++ uniqueNumberOfOccurences.py
@@ -0,0 +1,10 @@
+# Leetcode 1207
+
+from typing import List
+from collections import Counter
+
+class Solution:
+  def uniqueOccurrences(self, arr: List[int]) -> bool:
+    counter = Counter(arr)
+
+    return len(counter.keys()) == len(set(counter.values()))
