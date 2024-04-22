ab2389c5ecdd2bae126e02605d7c9322e378dfef
Sean Wong <sean.uuong@gmail.com>
2024-04-22 16:46:56 -0700

    Complete findNumbersWithEvenNumberOfDigits

---
+++ findNumbersWithEvenNumberOfDigits.py
@@ -0,0 +1,13 @@
+# Leetcode 1295
+
+from typing import List
+
+class Solution:
+  def findNumbers(self, nums: List[int]) -> int:
+    result = 0
+
+    for num in nums:
+      if len(str(num)) % 2 == 0:
+        result += 1
+
+    return result
