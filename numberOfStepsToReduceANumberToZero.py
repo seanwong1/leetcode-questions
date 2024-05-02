d0188b3ca6e0aa737d4d778fa041e844c4634034
Sean Wong <sean.uuong@gmail.com>
2024-05-01 17:19:10 -0700

    Complete numberOfStepsToReduceANumberToZero

---
+++ numberOfStepsToReduceANumberToZero.py
@@ -0,0 +1,15 @@
+# Leetcode 1342
+
+class Solution:
+  def numberOfSteps(self, num: int) -> int:
+    result = 0
+
+    while num > 0:
+      if num % 2 == 0:
+        num //= 2
+      else:
+        num -= 1
+
+      result += 1
+
+    return result
