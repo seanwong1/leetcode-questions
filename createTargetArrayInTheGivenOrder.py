bf681426f6f3cd1703ceee28e5531e6047c98b96
Sean Wong <sean.uuong@gmail.com>
2024-05-13 16:34:14 -0700

    Complete createTargetArrayInTheGivenOrder

---
+++ createTargetArrayInTheGivenOrder.py
@@ -0,0 +1,12 @@
+# Leetcdoe 1389
+
+from typing import List
+
+class Solution:
+  def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+    result = []
+
+    for i in range(len(nums)):
+      result.insert(index[i], nums[i])
+
+    return result
