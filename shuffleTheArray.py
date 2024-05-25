9f8d77e340d28c80f34144f42e36ade7728147cc
Sean Wong <sean.uuong@gmail.com>
2024-05-25 07:08:29 -0700

    Complete shuffleTheArray

---
+++ shuffleTheArray.py
@@ -0,0 +1,13 @@
+# Leetcode 1470
+
+from typing import List
+
+class Solution:
+  def shuffle(self, nums: List[int], n: int) -> List[int]:
+    result = []
+
+    for i in range(n):
+      result.append(nums[i])
+      result.append(nums[i + n])
+
+    return result
