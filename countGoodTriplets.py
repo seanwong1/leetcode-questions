43fa383f31e87af6635db6bc8d834cd29d32e3ab
Sean Wong <sean.uuong@gmail.com>
2024-06-06 10:36:07 -0700

    Complete countGoodTriplets

---
+++ countGoodTriplets.py
@@ -0,0 +1,16 @@
+# Leetcode 1534
+
+from typing import List
+
+class Solution:
+  def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+    result = 0
+
+    for i in range(len(arr)):
+      for j in range(i + 1, len(arr)):
+        if abs(arr[i] - arr[j]) <= a:
+          for k in range(j + 1, len(arr)):
+            if abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+              result += 1
+
+    return result
