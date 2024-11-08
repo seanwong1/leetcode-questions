392a817b776a5408b4e41b9a123ecd4fb1d7a191
Sean Wong <sean.uuong@gmail.com>
2024-11-07 16:57:35 -0800

    Complete numberOfStudentsDoingHomeworkAtAGivenTime

---
+++ numberOfStudentsDoingHomeworkAtAGivenTime.py
@@ -0,0 +1,13 @@
+# Leetcode 1450
+
+from typing import List
+
+class Solution:
+  def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+    count = 0
+
+    for i in range(len(startTime)):
+      if startTime[i] <= queryTime and endTime[i] >= queryTime:
+        count += 1
+
+    return count
